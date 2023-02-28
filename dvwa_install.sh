b20dd43209925b459b54f55a10bd9927b76536d9
Lavender Millstone <32195948+Lavender-exe@users.noreply.github.com>
2023-02-28 04:59:47 -0500

    Added DVWA script

---
+++ dvwa_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update -y && apt upgrade -y && apt dist-upgrade -y
+echo "[!]Installing Docker"
+sudo apt-get install docker docker.io
+
+echo "[!]Pulling Docker Container"
+sudo docker pull vulnerables/web-dvwa
+
+echo "[+] Adding to Aliases file"
+echo 'alias dvwa_host="sudo docker run --rm -it --detach -p 80:80 vulnerables/web-dvwa"' >> ~/.zshrc
+clear
+echo "Done! restart your terminal and write dvwa_host to host the webpage"
