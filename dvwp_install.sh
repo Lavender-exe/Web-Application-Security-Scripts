68854bf7a442f4d59023e0fcc24a7dd334a1c736
Lav <32195948+Lavender-exe@users.noreply.github.com>
2023-03-26 18:04:20 +0400

    Updated test

--- dvwp_install.sh
+++ dvwp_install.sh
@@ -2,16 +2,15 @@
 
 sudo apt update -y && apt upgrade -y && apt dist-upgrade -y
 echo "[!]Installing Docker"
-sudo apt-get install docker docker.io -y
+sudo apt-get install docker docker.io docker-compose -y
 
 echo "[!]Pulling Docker Container"
-cd ~/Documents/
 git clone https://github.com/vavkamil/dvwp.git
 cd dvwp/
 sudo docker-compose up -d --build
 sudo docker-compose run --rm wp-cli install-wp
 
-echo "[+] Adding to Aliases file"
-echo "alias dvwp_host='sudo docker-compose up ~/Documents/dvwp/'" >> ~/.zshrc
-clear
-echo "Done! restart your terminal and write dvwp_host to host the webpage. Website is hosted on http://127.0.0.1:31337/"
+echo "[!!] Installation Complete!
+Go to your installation directory 'dvwp'
+run command 'sudo docker-compose up' 
+Website is hosted on http://127.0.0.1:31337/"
