d065316f1d4dbd096720fa2618e18d42fe51f2a5
Lav <32195948+Lavender-exe@users.noreply.github.com>
2023-03-26 13:33:50 +0400

    Update dvwa_install.sh

--- dvwa_install.sh
+++ dvwa_install.sh
@@ -8,6 +8,6 @@ echo "[!]Pulling Docker Container"
 sudo docker pull vulnerables/web-dvwa
 
 echo "[+] Adding to Aliases file"
-echo 'alias dvwa_host="sudo docker run --rm -it --detach -p 80:80 vulnerables/web-dvwa"' >> ~/.zshrc
+echo "alias dvwa_host='sudo docker run --rm -it --detach -p 80:80 vulnerables/web-dvwa'" >> ~/.zshrc
 clear
 echo "Done! restart your terminal and write dvwa_host to host the webpage"
